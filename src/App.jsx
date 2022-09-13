ee7c85dd1e3b430e60423bf745c85f41e9985b44
Rlss91 <rlss91@gmail.com>
2022-09-13 11:10:49 +0300

    scss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+// import ShalomOlam, { f1, f2, f3 } from "./components/HelloWorld";
+import HelloWorld from "./components/HelloWorld";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
