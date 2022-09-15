81ba1197a18fb4568fcde5850da43d79f53516ea
Rlss91 <rlss91@gmail.com>
2022-09-15 10:02:34 +0300

    register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,23 @@
 import { Fragment, useState } from "react";
 const RegisterPage = () => {
-  const [nameInput, setNameInput] = useState("");
-
-  const handleNameInputChange = (event) => {
-    setNameInput(event.target.value);
+  const [stateNameInput, setNameInput] = useState(""); //init state
+  const [lastNameInput, setLastNameInput] = useState(""); //init state
+  const [emailInput, setEmailInput] = useState(""); //init state
+  const [passwordInput, setPasswordInput] = useState(""); //init state
+  const handleNameInputChange = (ev) => {
+    //this function made for the onChange event
+    //!stateNameInput = ev.target.value;//dont do this ever!!!!!!!!!!!!!!
+    setNameInput(ev.target.value); //change the value of the state
   };
-
+  const handleLastNameInputChange = (ev) => {
+    setLastNameInput(ev.target.value);
+  };
+  const handleEmailInputChange = (ev) => {
+    setEmailInput(ev.target.value);
+  };
+  const handlePasswordInputChange = (ev) => {
+    setPasswordInput(ev.target.value);
+  };
   return (
     <Fragment>
       <h2>Register page</h2>
@@ -15,7 +27,7 @@ const RegisterPage = () => {
           className="form-control"
           id="nameInput"
           placeholder="Name"
-          value={nameInput}
+          value={stateNameInput}
           onChange={handleNameInputChange}
         />
         <label htmlFor="nameInput">Name</label>
@@ -26,6 +38,10 @@ const RegisterPage = () => {
           className="form-control"
           id="lastNameInput"
           placeholder="Lastname"
+          value={lastNameInput}
+          onChange={(ev) => {
+            setLastNameInput(ev.target.value);
+          }}
         />
         <label htmlFor="lastNameInput">Lastname</label>
       </div>
@@ -35,6 +51,8 @@ const RegisterPage = () => {
           className="form-control"
           id="floatingInput"
           placeholder="name@example.com"
+          value={emailInput}
+          onChange={handleEmailInputChange}
         />
         <label htmlFor="floatingInput">Email address</label>
       </div>
@@ -44,6 +62,8 @@ const RegisterPage = () => {
           className="form-control"
           id="floatingPassword"
           placeholder="Password"
+          value={passwordInput}
+          onChange={handlePasswordInputChange}
         />
         <label htmlFor="floatingPassword">Password</label>
       </div>
