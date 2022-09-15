e36cf3de215198a1d8fb04d1af3f92d0f97285c5
Rlss91 <rlss91@gmail.com>
2022-09-15 15:55:31 +0300

    sideEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.scss";
 // import ShalomOlam, { f1, f2, f3 } from "./components/HelloWorld";
 import HelloWorldComponent from "./components/HelloWorldComponent";
@@ -5,15 +6,19 @@ import LoginPage from "./pages/LoginPage";
 import ConditionPage from "./pages/ConditionPage";
 import RegisterPage from "./pages/RegisterPage";
 import PanelPage from "./pages/PanelPage/PanelPage";
+import SideEffectPage from "./pages/SideEffect";
 
 const App = () => {
+  const [tf, setTf] = useState(true);
   return (
     <div className="container">
       {/* <HelloWorldComponent /> */}
       {/* <RegisterPage /> */}
       {/* <LoginPage /> */}
       {/* <ConditionPage /> */}
-      <PanelPage />
+      {/* <PanelPage /> */}
+      <button onClick={(ev) => setTf(!tf)}>toggle timer</button>
+      {tf && <SideEffectPage />}
     </div>
   );
 };
