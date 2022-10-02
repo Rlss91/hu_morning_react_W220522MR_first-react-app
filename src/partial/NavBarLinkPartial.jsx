bfc7e5e789ad1a72b36fe50964e85641d0bcf967
Rlss91 <rlss91@gmail.com>
2022-10-02 13:49:33 +0300

    better nav

--- src/partial/NavBarLinkPartial.jsx
+++ src/partial/NavBarLinkPartial.jsx
@@ -3,10 +3,38 @@ import { NavLink } from "react-router-dom";
 const NavBarLinkPartial = ({ label, link }) => {
   return (
     <li className="nav-item">
-      <NavLink className="nav-link" to={link}>
+      <NavLink
+        className="nav-link"
+        to={link}
+        isActive={(match, location) => match && match.isExact}
+      >
         {label}
       </NavLink>
     </li>
   );
 };
 export default NavBarLinkPartial;
+// const NavBarLinkPartial = ({ label, link }) => {
+//   return (
+//     <li className="nav-item">
+//       <NavLink
+//         className="nav-link"
+//         to={link}
+//         isActive={(match, location) => {
+//           // console.log("match", match);
+//           // console.log("location", location);
+//           if (match === null) {
+//             return false;
+//           }
+//           if (match.isExact === false) {
+//             return false;
+//           }
+//           return true;
+//         }}
+//       >
+//         {label}
+//       </NavLink>
+//     </li>
+//   );
+// };
+// export default NavBarLinkPartial;
